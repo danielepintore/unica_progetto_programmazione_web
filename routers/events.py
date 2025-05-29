b7884e5fa446bfcd50990e6e03104b0dee6e3db5
Ludovica Cadoni <ludovicaa.cadoni@gmail.com>
2025-05-29 12:18:55 +0200

    put added

--- routers/events.py
+++ routers/events.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, HTTPException
 from data.db import DBSession
 from sqlmodel import select, delete
-from models.events import Event
+from models.events import Event, EventCreate
+from typing import Annotated
 
 router = APIRouter()
 
@@ -24,3 +25,20 @@ def delete_all_events(db_session: DBSession):
    db_session.exec(statement)
    db_session.commit()
    return "All events deleted successfully!"
+
+@router.put("/events/{id}")
+def update_event(
+    db_session: DBSession,
+    id: Annotated[int, Path(description="The id of the event to update")],
+    new_event: EventCreate
+):
+    event = db_session.get(Event, id)
+    if event is None:
+        raise HTTPException(status_code=404, detail="Event not found")
+    event.title = new_event.title
+    event.description = new_event.description
+    event.date = new_event.date
+    event.location = new_event.location
+    db_session.add(event)
+    db_session.commit()
+    return "Event updated successfully!"
