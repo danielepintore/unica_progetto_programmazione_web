d0209ad55c6881fe5d35af5ba12a35d3c1ddc1df
Daniele Pintore <daniele.pintore1@gmail.com>
2025-05-29 17:24:21 +0200

    Add db constraints (we can't have the same user register twice)

--- models/registrations.py
+++ models/registrations.py
@@ -2,11 +2,10 @@ from sqlmodel import SQLModel, Field
 
 
 class RegistrationsBase(SQLModel):
-    username: str
     name: str
-    event_id: int
 
 
 # Modello relazionale (dati non validati -> table=True)
 class Registrations(RegistrationsBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(default=None, primary_key=True)
+    event_id: int = Field(default=None, primary_key=True)
