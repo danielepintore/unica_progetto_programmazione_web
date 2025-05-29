6ff3b07a37419c29545b9d0bd153940e583992ef
Daniele Pintore <daniele.pintore1@gmail.com>
2025-05-29 17:08:45 +0200

    refactor: move api to /v1/ path

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from contextlib import asynccontextmanager
+from data.db import init_database
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
-from contextlib import asynccontextmanager
 from routers import frontend, events, users, registrations
-from data.db import init_database
 
 API_PREFIX = "/v1"
 APP_STATIC_DIR = Path(__file__).parent / "static"
@@ -18,8 +18,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(frontend.router)
-app.include_router(events.router)
-app.include_router(users.router)
-app.include_router(registrations.router)
+app.include_router(events.router, prefix=API_PREFIX)
+app.include_router(users.router, prefix=API_PREFIX)
+app.include_router(registrations.router, prefix=API_PREFIX)
 # app.include_router(frontend.router, prefix=API_PREFIX)
 app.mount("/", StaticFiles(directory=APP_STATIC_DIR, html=True), name="static")
