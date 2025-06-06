14e672de115baf872b2ca17b0d44477628133173
Daniele Pintore <daniele.pintore1@gmail.com>
2025-06-06 13:12:39 +0200

    Implement custom validator since the date is in a format without the seconds

--- models/events.py
+++ models/events.py
@@ -1,13 +1,24 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 from pydantic import Field as PyField
+from pydantic import field_validator
 
 
 class EventBase(SQLModel):  # Superclasse con attibuti comuni
-    title: str
-    description: str
+    title: str = PyField(min_length=3)
+    description: str = PyField(min_length=3)
     date: datetime = PyField(strict=True)
-    location: str
+    location: str = PyField(min_length=3)
+
+    @field_validator('date', mode='before')
+    @classmethod
+    def parse_custom_datetime(cls, v):
+        if isinstance(v, str):
+            try:
+                return datetime.strptime(v, '%Y-%m-%dT%H:%M')
+            except ValueError:
+                raise ValueError('Datetime must be in format YYYY-MM-DDTHH:MM')
+        return v
 
 
 # Modello relazionale (dati non validati -> table=True)
