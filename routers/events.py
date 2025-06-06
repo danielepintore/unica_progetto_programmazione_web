935cf24c74027f8752b4a1e620f3ea3aa94a3441
Daniele Pintore <daniele.pintore1@gmail.com>
2025-06-06 13:37:31 +0200

    Add validation for editing and event

--- routers/events.py
+++ routers/events.py
@@ -74,9 +74,11 @@ def update_event(
         event.description = new_event.description
         event.date = new_event.date
         event.location = new_event.location
-        db_session.add(event)
+        db_session.add(Event.model_validate(event))
         db_session.commit()
         return "Event updated successfully!"
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=e.__str__())
     except Exception as e:
         raise HTTPException(status_code=500, detail=e.__str__())
 
